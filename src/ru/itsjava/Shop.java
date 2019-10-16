af467ff251a3f85153d61eb13490bdd26b445df9
Vitalij Nezhinskij <SeekerOfConfessor@gmail.com>
2019-10-17 01:47:13 +0300

    lesson 8, realized part of menu

--- src/ru/itsjava/Shop.java
+++ src/ru/itsjava/Shop.java
@@ -1,5 +1,6 @@
 package ru.itsjava;
 
+import java.util.Arrays;
 import java.util.Scanner;
 
 /**
@@ -14,16 +15,27 @@ public class Shop {
         String enterNumOfMenu = "Введите номер меню от 0 до 7.";
         Scanner scanner = new Scanner(System.in);
         int numOfMenu;
-
+        String[] courses = new String[]{"Java для всех", "Мотивация, когда ты упал",
+                "Продай другому и не подай себя",
+                "Наготочки и пилочки для чайников",
+                "Тортики или как я заработал миллион"};
         printMenu(enterNumOfMenu);
         while ((numOfMenu = scanner.nextInt()) != 0) {
-            printMenu(enterNumOfMenu);
             if ((numOfMenu < 0) || (numOfMenu > 7)) {
                 System.out.println("Вы ввели некорректный номер меню (" + numOfMenu
                         + ")." + enterNumOfMenu);
 //                System.exit(-1);
             }
 
+            switch (numOfMenu) {
+                case 1:
+                    showAllCourses(courses);
+                    break;
+                case 2:
+                    courses = addCourse(scanner, courses);
+                    break;
+            }
+            printMenu(enterNumOfMenu);
         }
         System.out.println("Спасибо, что воспользовались нашим магазином." +
                 "Учите Java! И разрабатывайте свой магазин. " +
@@ -31,7 +43,24 @@ public class Shop {
         scanner.close();
     }
 
+    private static String[] addCourse(Scanner scanner, String[] courses) {
+        String[] resCourses = Arrays.copyOf(courses,courses.length +1);
+        System.out.println("Введи новое имя курса:");
+        String inputCourse = scanner.next();
+        resCourses[resCourses.length - 1] = inputCourse;
+        System.out.println("Курс " + inputCourse +" успешно добавлен!!");
+        return resCourses;
+    }
+
+    private static void showAllCourses(String[] courses) {
+        System.out.println("Вывод всех курсов:");
+        for (String course : courses) {
+            System.out.println(course);
+        }
+    }
+
     private static void printMenu(String enterNumOfMenu) {
+        //charAt()
         System.out.println("Меню:\n" +
                 "1. Показать все курсы.\n" +
                 "2. Добавить курс.\n" +
@@ -42,5 +71,4 @@ public class Shop {
                 "7. Придумать самим.\n" +
                 "0. Выход.\n" + enterNumOfMenu);
     }
-
 }
